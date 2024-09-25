6688332d9d2ba07209c6885c35cc21c3bc403528
CodeNameHealer <swagatprasad3344@gmail.com>
2024-09-25 20:00:07 +0530

    added the table and local storage is working

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -50,9 +50,9 @@ const Manager = () => {
                 <p className="text-green-900 text-lg text-center">
                     Your own password Manager
                 </p>
-                <div className="text-white flex flex-col p-4 text-black gap-8 items-center">
+                <div className="text-white flex flex-col p-4 gap-8 items-center">
                     <input
-                        className="rounded-full border border-green-500 w-full p-4 py-1"
+                        className="rounded-full border border-green-500 w-full p-4 py-1 text-black"
                         value={form.site}
                         onChange={handleChange}
                         placeholder="Enter Webiste URL"
@@ -62,7 +62,7 @@ const Manager = () => {
                     />
                     <div className="flex w-full justify-between gap-8 ">
                         <input
-                            className="rounded-full border border-green-500 w-full p-4 py-1"
+                            className="rounded-full border border-green-500 w-full p-4 py-1 text-black"
                             value={form.username}
                             onChange={handleChange}
                             placeholder="Enter Username"
@@ -72,7 +72,7 @@ const Manager = () => {
                         />
                         <div className="relative">
                             <input
-                                className="rounded-full border border-green-500 w-full p-4 py-1"
+                                className="rounded-full border border-green-500 w-full p-4 py-1 text-black"
                                 value={form.password}
                                 onChange={handleChange}
                                 placeholder="Enter Password"
@@ -92,33 +92,27 @@ const Manager = () => {
                         </lord-icon>Add Password</button>
                 </div>
                 <div className="passwords">
-                    <h2>Your Passwords</h2>
-                    <table className="table-auto">
-                        <thead>
+                    <h2 className="font-bold text-2xl py-4">Your Passwords</h2>
+                    {passwordArray.length === 0 && <div>No Passwords to Show</div>}
+                    {passwordArray.length != 0 && <table className="table-auto w-full rounded-md overflow-hidden">
+                        <thead className="bg-green-800 text-white">
                             <tr>
-                                <th>Song</th>
-                                <th>Artist</th>
-                                <th>Year</th>
+                                <th className="py-2">Site</th>
+                                <th className="py-2">Username</th>
+                                <th className="py-2">Password</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr>
-                                <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                                <td>Malcolm Lockyer</td>
-                                <td>1961</td>
-                            </tr>
-                            <tr>
-                                <td>Witchy Woman</td>
-                                <td>The Eagles</td>
-                                <td>1972</td>
-                            </tr>
-                            <tr>
-                                <td>Shining Star</td>
-                                <td>Earth, Wind, and Fire</td>
-                                <td>1975</td>
-                            </tr>
+                        <tbody className="bg-green-100">
+                            {passwordArray.map((item, index) => {
+                                return <tr key={index}>
+                                    <td className="text-center w-32 py-2 border border-white"><a href={item.site} target="_blank">{item.site}</a></td>
+                                    <td className="text-center w-32 py-2 border border-white">{item.username}</td>
+                                    <td className="text-center w-32 py-2 border border-white">{item.password}</td>
+                                </tr>
+                            })}
                         </tbody>
                     </table>
+                    }
                 </div>
             </div>
         </>
